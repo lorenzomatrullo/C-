7e63b60e786a0368568dbb705957bf00042040a1
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-09 14:29:06 +0100

    typedef and using aliases

---
+++ lib/typedef_and_using.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+/*
+    Typedef = reserved keyword used to create an additional name for another data type.
+    New identifier for an existing type.
+    Helps with readability and reduces typos.
+    Mostly Repleace with "using" (work better w/ templates)
+*/
+
+
+
+// typedef std::string text_t;
+// _t at the end of the keyword is used as a convention for typedefs
+using text_t = std::string;
+
+// typedef int number_t;
+using number_t = int;
+
+
+
+int main(){
+
+    text_t name = "Lorenzo";
+    number_t age = 19;
+
+    std::cout << name << '\n';
+    std::cout << age;
+
+    return 0;
+}
