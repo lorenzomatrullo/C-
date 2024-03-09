5986e09b81078e7dc4782812fbf32ec6ab5b61ca
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-09 14:55:12 +0100

    math operators

--- lib/math_operators.cpp
+++ lib/math_operators.cpp
@@ -1,2 +1,35 @@
 #include <iostream>
 
+/*
+    + (addition)
+    - (subtraction)
+    * (moltiplication)
+    / (division)
+    % (modulus) means  "the rest of a division"
+    ++ (increment)
+    -- (decrement)
+*/
+
+
+int main(){
+
+    int x = 10;
+
+    // x = x + 1;
+    x += 1;
+
+    // x = x - 1;
+    x -= 1;
+
+    // x = x * 2;
+    x *= 2;
+
+    // x = x / 2;
+    x /= 2;
+
+    x = x % 2;
+    x %= 2;
+
+
+    return 0;
+}
