dec77e41a764c9f49667d26f432fb5c411444e35
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-13 12:37:26 +0100

    Random Number

---
+++ lib/random_numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime> // Include the <ctime> header for time()
+
+using namespace std;
+
+int main() {
+    // Seed the random number generator with the current time
+    srand((time(0)));
+
+    // Generate a random number
+    int randomNumber = (rand() % 100) + 1;
+
+    cout << "Random number: " << randomNumber;
+
+    return 0;
+}
