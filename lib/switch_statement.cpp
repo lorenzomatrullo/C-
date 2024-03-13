0bbbc018505e6ec76aa9d3bb2c73983357ee50d2
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-13 11:42:29 +0100

    switch statement

---
+++ lib/switch_statement.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    SWITCH = an alternative way of using "else if" statements, compares one value gainst matching cases
+*/
+
+int main(){
+
+    int month;
+    cout << "Enter a month (1-12)";
+    cin >> month;
+
+
+    switch(month){
+        case 1:
+            cout << "January";
+            break;
+        case 2:
+            cout << "February";
+            break;
+        case 3:
+            cout << "March";
+            break;
+        case 4:
+            cout << "April";
+            break;
+        case 5:
+            cout << "May";
+            break;
+        case 6:
+            cout << "June";
+            break;
+        case 7:
+            cout << "July";
+            break;
+        case 8:
+            cout << "August";
+            break;
+        case 9:
+            cout << "September";
+            break;
+        case 10:
+            cout << "October";
+            break;
+        case 11:
+            cout << "November";
+            break;
+        case 12:
+            cout << "December";
+            break;
+        default:
+            cout << "Please enter only number: ";
+    }    
+
+
+    return 0;
+}
