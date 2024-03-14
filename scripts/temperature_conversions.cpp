a5d17169aa867cb477822873923031af4ece73e6
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-14 10:35:00 +0100

    formatting

--- scripts/temperature_conversions.cpp
+++ scripts/temperature_conversions.cpp
@@ -9,24 +9,25 @@ int main(){
     double temp;
 
     cout << "*****TEMPERATURE CONVERSION*****\n";
-
-    cout << "Is the temperature in (F) or (C): ";
+    cout << "F = Fahrenheit\n";
+    cout << "C = Celsius\n";
+    cout << "What unit would you like to convert to (F) or (C): ";
     cin >> unit;
 
     unit = toupper(unit); // lowercase letters will be turned into uppercase
 
     if(unit == 'C'){
-        cout << "Enter the temperature in Celcius: ";
-        cin >> temp;
-
-        temp = (temp * 9 / 5) + 32;
-        cout << "The temperature in Farenheit is: " << temp;
-    } else if (unit == 'F'){
         cout << "Enter the temperature in Farenheit: ";
         cin >> temp;
 
+        temp = (temp * 9 / 5) + 32;
+        cout << "The temperature is: " << temp << "C";
+    } else if (unit == 'F'){
+        cout << "Enter the temperature in Celsius: ";
+        cin >> temp;
+
         temp = ((temp - 32) * 5) / 9;
-        cout << "The temperature in Celsius is: " << temp;
+        cout << "The temperature is: " << temp << "F";
     } else {
         cout << "That is not a valid unit of measurement";
     }
