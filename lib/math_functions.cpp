ad07b3f7854b2622facfbc8a07325046826b0cc6
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-15 18:54:17 +0100

    math functions

---
+++ lib/math_functions.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    double A = sqrt(9);             // square root of
+    double B = pow(2, 4);           // to the power of
+    int C = round(3.14);            // rounds a number
+    int D = ceil(3.14);             // rounds a number up
+    int E = floor(3.99);            // rounds a number down
+    double F = fabs(-100);          // absolute value of a number
+    double G = log(3);              // logarithm
+    double H = sin(45);             // sine
+    double I = cos(45);             // cosine
+    double J = tan(45);             // tangent
+    
+    cout << A;
+
+    return 0;
+}
