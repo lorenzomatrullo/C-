358524a6fbf2cd17aec3630873588be101a59783
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-15 11:44:37 +0100

    for loop

---
+++ lib/for_loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    FOR LOOP = repeats a section of code a limited amount of times
+
+    1st statement = starting from
+    2nd statement = arriving to
+    3rd stament = whats the process between
+*/
+
+int main(){
+
+    for(int i = 0; i < 5; i++){
+        cout << "Hello World\n";
+    }
+
+
+    return 0;
+}
