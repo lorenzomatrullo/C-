6343ef96cd06841eee065a7d389e4368d032f201
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-16 14:07:51 +0100

    local and global variables

---
+++ lib/variables.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    VARIABLE = Allocated space in memory to store a value. 
+    We refer to a variable's name to access the stored value.
+    That variable now behaves as if it was the value it contains.
+    BUT we need to declare what type of data we are storing.
+*/
+
+/*
+    Local variables = declared inside a function or block {}
+    Global variables = declared outside of all functions
+*/
+
+int myNum = 2;          // Global variable
+
+void printNum();
+
+int main(){
+
+    int x;              // declaration
+    x = 123;            // initialization
+    int y = 321;        // declaration + initialization
+
+    int myNum = 0;      // Local variable
+
+    printNum();
+
+    return 0;
+}
+
+void printNum(){
+    int myNum = 1;      // Local variable
+    cout << ::myNum;    // Precede the variable name with the scope resolution operator (::) if you'd rather use the global  variable 
+}
