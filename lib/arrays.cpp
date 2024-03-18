87724dd5d45b296dbaa413a3043d73608a6f7179
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-18 18:24:38 +0100

    arrays

---
+++ lib/arrays.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// array = a data structure that can store many values of the same data type;
+
+int main(){
+
+    double prices[4];
+
+    prices[0] = 5.0;
+    prices[1] = 10.0;
+    prices[2] = 15.0;
+    prices[3] = 20.0;
+    prices[4] = 25.0;
+
+    string cars[] = {"Corvette", "Mustang", "Camry"};
+
+    cout << cars[0] << '\n';
+    cout << cars[1] << '\n';
+    cout << cars[2] << '\n';
+
+    return 0;
+}
