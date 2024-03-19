cbff0447ef845bb90abc891500f71c2df7d6a12e
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-19 09:47:38 +0100

    cin and getline functions

--- lib/escape_sequences.cpp
+++ lib/escape_sequences.cpp
@@ -36,5 +36,14 @@ int main(){
     // << output
     // >> input
 
+    ///////////////////////////
+    string full_name;
+    cout << "Enter your full name: ";
+
+    // cin >> full_name;                  // will only display the first word
+    getline(cin, full_name);              // will display the entire string (can only be used with string function because it is a C++ function)
+
+    cout << "You entered: " << full_name;
+
     return 0;
 }
