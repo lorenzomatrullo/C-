06e7c1b97e5e372dbf566f68c3951b9ea9c0e646
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-03-21 12:29:19 +0100

    constructors

---
+++ lib/constructors.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+/*
+    CONSTRUCTOR = special method that is automatically called when an object is instantiated useful for assigning values to attributes as arguments.
+*/
+
+
+class Student {
+    public:
+        string name;
+        int age;
+        double gpa;
+
+    Student(string name, int age, double gpa){
+        this -> name = name;
+        this -> age = age;
+        this -> gpa = gpa;
+    }
+};
+
+class Car{
+    public:
+        string make;
+        string model;
+        int year;
+        string color;
+
+    Car(string make, string model, int year, string color){
+        this -> make = make;
+        this -> model = model;
+        this -> year = year;
+        this -> color = color;
+    }
+};
+
+
+int main(){
+
+    Student student1("Spongebob", 25, 3.2);
+
+    cout << student1.name << '\n';
+    cout << student1.age << '\n';
+    cout << student1.gpa << '\n';
+
+    cout << '\n';
+
+    Car car1("Chevy", "Corvette", 2022, "blue");
+
+    cout << car1.make << '\n';
+    cout << car1.model << '\n';
+    cout << car1.year << '\n';
+    cout << car1.color << '\n';
+
+
+    return 0;
+}
