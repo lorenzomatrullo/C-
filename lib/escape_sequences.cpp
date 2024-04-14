9d147f3fb225bfd576cb49cb4dca758aee1a4e54
lorenzomatrullo <lorenzomatrullo04@gmail.com>
2024-04-14 16:53:51 +0200

    formatting & #include, #define, #undef, #ifdef, #ifndef, #else, #elif, #endif

--- lib/escape_sequences.cpp
+++ lib/escape_sequences.cpp
@@ -32,11 +32,14 @@ int main(){
     cout << "Hello World" << '\n';
 
 
-    // flow operators
-    // << output
-    // >> input
+    /*
+        FLOW OPERATORS:
+        << output
+        >> input
+    */
 
     ///////////////////////////
+
     string full_name;
     cout << "Enter your full name: ";
 
@@ -44,6 +47,70 @@ int main(){
     getline(cin, full_name);              // will display the entire string (can only be used with string function because it is a C++ function)
 
     cout << "You entered: " << full_name;
+
+    ///////////////////////////
+
+    /*
+        #include <iostream>       
+            *Searches for the header file in standard library directories.
+            *Use this for including headers that are part of the C++ standard library or system-specific libraries.
+        
+        #include "header.cpp"
+            *Searches for the header file within the current project's include directories.
+            *Use this for including your own custom header files or headers from other parts of your project.
+    */
+
+
+   /*
+        #define
+            *Defines a macro that can be used throughout the code.
+            #define MACRO_NAME value    (Uppercase convention is common)
+            #define DEBUG               (Simply defines a macro named DEBUG (no value))
+            #define PI 3.14159          (Assigns the value 3.14159 to the macro PI)
+
+
+        #undef
+            *Undefines the macro previously assigned.
+            #undef DEBUG                (DEBUG is no longer available after this point)
+
+
+        #ifdef
+            *Checks if a macro has been previously defined with #define
+            #ifdef MACRO_NAME
+            If MACRO_NAME is defined, the code block following #ifdef is included for compilation.
+            If MACRO_NAME is NOT defined, the code block is skipped. 
+
+            #ifdef DEBUG
+                cout << "Debug message" << endl;
+            #endif
+
+
+        #ifndef
+            *The opposite of #ifdef
+            #ifndef MACRO_NAME
+            If MACRO_NAME is NOT defined, the code block following #ifndef is included for compilation.
+            If MACRO_NAME is defined, the code block is skipped.
+
+        
+        #else
+            *Used with #ifdef or #ifndef to provide alternative code if the initial condition is not met.
+
+        
+        #elif (else if)
+            *Used with #ifdef, #ifndef, or other #elif directives for multiple conditional checks.
+
+            #ifdef FEATURE_A
+                // Code for feature A
+            #elif FEATURE_B
+                // Code for feature B
+            #else
+                // No features enabled
+            #endif
+
+
+        #endif
+            *Marks the end of a conditional compilation block started by #ifdef, #ifndef, #elif, or #else.
+   */
 
     return 0;
 }
