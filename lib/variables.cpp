b0449c998273773ab0ba9cf74dad041d63f56d3b
Relique <lorenzomatrullo04@gmail.com>
2024-04-21 18:40:07 +0200

    Update variables.cpp

--- lib/variables.cpp
+++ lib/variables.cpp
@@ -7,6 +7,8 @@ using namespace std;
     We refer to a variable's name to access the stored value.
     That variable now behaves as if it was the value it contains.
     BUT we need to declare what type of data we are storing.
+
+    const = a variable that can't be changed
 */
 
 /*
